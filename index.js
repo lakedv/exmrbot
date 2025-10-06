fe6ce422ecf1d3730bcf4b178eabc310caacc53f
lakedv <uaterules@gmail.com>
2025-10-06 00:15:11 -0300

    VIEW+README

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const { port } = require('./config/config')
 const authRoutes = require('./routes/authRoutes')
 const weatherRoutes = require('./routes/weatherRoutes');
+const cors = require('cors');
+
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/auth', authRoutes);
 app.use('/weather', weatherRoutes)
